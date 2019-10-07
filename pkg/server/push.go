4249c8d2a03b7152f23d83c3e92c1d243c9a9796
Stefan Majer <stefan.majer@f-i-ts.de>
2019-10-07 15:28:50 +0200

    Log drop contextual

--- pkg/server/push.go
+++ pkg/server/push.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -12,6 +13,12 @@ type Server struct{}
 
 // Push implements droptailer.Push
 func (s *Server) Push(ctx context.Context, de *pb.Drop) (*pb.Void, error) {
-	fmt.Printf("%s %s\n", time.Unix(de.Timestamp.Seconds, 0), de.Fields)
+	de.Fields["timestamp"] = fmt.Sprintf("%s", time.Unix(de.Timestamp.Seconds, 0))
+
+	js, err := json.Marshal(de.Fields)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("%s %s\n", de.Fields["timestamp"], js)
 	return &pb.Void{}, nil
 }
